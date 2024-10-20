079567b863f2d649709a8046d68bba93404527c6
Yehan De Silva <yehan@fiberdock.ca>
2024-10-20 13:36:26 -0400

    Initial setup of project
    Setup project structure and boilerplate code.

---
+++ backend/src/main/java/sysc4806group25/monkeypoll/MonkeyPollApplication.java
@@ -0,0 +1,13 @@
+package sysc4806group25.monkeypoll;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MonkeyPollApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MonkeyPollApplication.class, args);
+    }
+
+}
---
+++ backend/src/test/java/sysc4806group25/monkeypoll/MonkeyPollApplicationTests.java
@@ -0,0 +1,13 @@
+package sysc4806group25.monkeypoll;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MonkeyPollApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
