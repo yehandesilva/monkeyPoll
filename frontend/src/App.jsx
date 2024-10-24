34eb38bf0dd2073467cfa05ac201e12483856ec2
Yehan De Silva <yehan@fiberdock.ca>
2024-10-24 19:46:53 -0400

    Moved login button to menu bar

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -8,7 +8,7 @@ const App = () => {
 
     return (
         <>
-            <Welcome />
+            <Welcome/>
         </>
     )
 }
--- frontend/src/welcome/Welcome.jsx
+++ frontend/src/welcome/Welcome.jsx
@@ -2,14 +2,14 @@ import {useState} from 'react';
 
 import {Button} from 'primereact/button';
 import {InputText} from 'primereact/inputtext';
+import MenuBar from "../main/MenuBar.jsx";
 
 const Welcome = () => {
     const [code, setCode] = useState("");
 
     return (
         <div className="h-screen w-full relative" style={{backgroundColor: '#014F5E'}}>
-            <Button label="Login" icon="pi pi-sign-in" size="small" className="absolute top-0 right-0 m-4" style={{boxShadow: "none"}} />
-
+            <MenuBar/>
             <div className="flex flex-column justify-content-center align-items-center gap-4 h-full">
                 <img src="monkeyPollLogoWithWhiteText.png" alt="MonkeyPoll Logo"
                      style={{width: '90%', maxWidth: '1080px', height: 'auto'}}/>
