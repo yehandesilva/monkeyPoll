f0805dbb192685e2539d3039729a481ccb24057e
Yehan De Silva <yehan@fiberdock.ca>
2024-10-25 11:34:37 -0400

    Added Login Component

---
+++ frontend/src/welcome/Login.jsx
@@ -0,0 +1,73 @@
+import {useState, useRef} from 'react';
+
+import {Toast} from 'primereact/toast';
+import {InputText} from 'primereact/inputtext';
+import {Password} from 'primereact/password';
+import {Button} from 'primereact/button';
+
+const Login = ({setVisible}) => {
+
+    const toast = useRef(null);
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+
+    const onSubmit = () => {
+        if (email && password) {
+            // Validate email format
+            const emailRegX = /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i;
+            if (emailRegX.test(email)) {
+                // TODO Sign-in user
+                setVisible(false);
+            } else {
+                toast.current.show({
+                    severity: 'error',
+                    life: 3000,
+                    summary: 'Login Error',
+                    detail: 'Email must be in format of example@email.com'
+                });
+            }
+        } else {
+            // Email or password fields are empty
+            if (!email) {
+                toast.current.show({
+                    severity: 'error',
+                    life: 3000,
+                    summary: 'Login Error',
+                    detail: 'Email is required'
+                });
+            }
+            if (!password) {
+                toast.current.show({
+                    severity: 'error',
+                    life: 3000,
+                    summary: 'Login Error',
+                    detail: 'Password is required'
+                });
+            }
+        }
+    }
+
+    return (
+        <>
+            <Toast ref={toast}/>
+            <div className="flex justify-content-center pb-6">
+                <div className="flex flex-column max-w-max">
+                    <div>
+                        <label htmlFor="email" className="block mb-1 ml-1">E-mail</label>
+                        <InputText value={email} onChange={(e) => setEmail(e.target.value)} className="w-full"/>
+                    </div>
+                    <div className="mt-3">
+                        <label htmlFor="password" className="block mb-1 ml-1">Password</label>
+                        <Password value={password} toggleMask feedback={false}
+                                  onChange={(e) => setPassword(e.target.value)}/>
+                    </div>
+                    <div className="flex justify-content-center mt-6">
+                        <Button label="Login" icon="pi pi-sign-in" onClick={() => onSubmit()} className="max-w-min"/>
+                    </div>
+                </div>
+            </div>
+        </>
+    )
+}
+
+export default Login;
--- frontend/src/welcome/LoginDialog.jsx
+++ frontend/src/welcome/LoginDialog.jsx
@@ -3,13 +3,15 @@ import {useState} from 'react';
 import {Dialog} from 'primereact/dialog';
 import {Button} from 'primereact/button';
 
+import Login from "./Login.jsx";
+
 const LoginDialog = ({visible, setVisible}) => {
 
     const [isLogin, setIsLogin] = useState(true);
 
     return (
         <>
-            <Dialog visible={visible} style={{width: '75vw'}} onHide={() => {
+            <Dialog visible={visible} style={{minWidth: '30vw'}} draggable={false} resizable={false} onHide={() => {
                 setVisible(false);
             }}>
                 <div className="flex flex-column gap-2">
@@ -17,11 +19,18 @@ const LoginDialog = ({visible, setVisible}) => {
                         <img src="monkeyPollLogoWithWhiteText.png" alt="MonkeyPoll Logo"
                              style={{width: '80%', maxWidth: '400px', height: 'auto'}}/>
                     </div>
-                    <div className="flex justify-content-center align-items-center">
-                        <span>{(isLogin) ? "Don't have an account?" : "Already have an account?"}</span>
+                    <div className="flex justify-content-center align-items-center mb-5">
+                        <span
+                            className="font-medium">{(isLogin) ? "Don't have an account?" : "Already have an account?"}</span>
                         <Button text onClick={() => setIsLogin(!isLogin)} className="px-2"
                                 style={{boxShadow: "none"}}>{(isLogin) ? "Create!" : "Login!"}</Button>
                     </div>
+                    {
+                        (isLogin) ?
+                            <Login setVisible={setVisible}/>
+                            :
+                            null
+                    }
                 </div>
             </Dialog>
         </>
