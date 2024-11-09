ee3a56c401849bdca13338d277f9c5181dea1464
Amin Zeina <36240585+AminZeina@users.noreply.github.com>
2024-11-09 13:24:06 -0500

    Fixed circular dependency

--- backend/src/main/java/sysc4806group25/monkeypoll/service/AccountUserDetailsService.java
+++ backend/src/main/java/sysc4806group25/monkeypoll/service/AccountUserDetailsService.java
@@ -1,6 +1,7 @@
 package sysc4806group25.monkeypoll.service;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Lazy;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
@@ -20,6 +21,7 @@ public class AccountUserDetailsService implements UserDetailsService {
     private AccountRepository accountRepository;
 
     @Autowired
+    @Lazy
     private PasswordEncoder passwordEncoder;
 
     @Override
