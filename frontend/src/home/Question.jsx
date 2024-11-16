2756029f43c650d519b61efdf766490055ffc99f
Amin Zeina <36240585+AminZeina@users.noreply.github.com>
2024-11-15 19:36:59 -0500

    Added incomplete question component and button to add Questions to CreateSurvey

---
+++ frontend/src/home/Question.jsx
@@ -0,0 +1,58 @@
+import { useState } from 'react';
+import { InputText } from 'primereact/inputtext';
+import {Card} from "primereact/card";
+import {Dropdown} from "primereact/dropdown";
+import {Slider} from "primereact/slider";
+import PropTypes from "prop-types";
+
+const Question = ({id}) => {
+    const [prompt, setPrompt] = useState()
+    const [type, setType] = useState()
+    const [responseOptions, setResponseOptions] = useState()
+
+    const questionTypes = [
+        {label: "Text Question", value: "text"},
+        {label: "Number Question", value: "number"},
+        {label: "Choice Question", value: "choice"}
+    ]
+
+    const renderTypeSelection = () => {
+        switch(type) {
+            case "text":
+                return <InputText disabled placeholder="No response options needed" className="w-full m-auto"/>
+            case "number":
+                return <Slider value={responseOptions} onChange={(e) => setResponseOptions(e.value)} className="m-4 w-auto" range />
+            case "choice":
+                return <InputText placeholder="Option 1" />
+            default:
+                return null;
+
+
+        }
+    }
+
+    return (
+      <>
+          <Card title={"Question " + id} className="w-6">
+              <div className="formgrid grid">
+                  <div className="field col-12 md:col-6">
+                      <InputText type="text" placeholder="Enter question prompt" className="w-full"/>
+                  </div>
+                  <div className="field col-12 md:col-6">
+                      <Dropdown value={type} onChange={(e) => setType(e.value)} options={questionTypes}
+                                placeholder="Select a question type" className="w-full"/>
+                  </div>
+                  <div className="field col-12">
+                      {renderTypeSelection()}
+                  </div>
+              </div>
+          </Card>
+      </>
+)
+}
+
+Question.propTypes = {
+    id: PropTypes.number.isRequired
+};
+
+export default Question
