91a766beea7fe1c8537066d785e90d2218d2e195
Amin Zeina <36240585+AminZeina@users.noreply.github.com>
2024-11-15 19:36:59 -0500

    Added incomplete question component and button to add Questions to CreateSurvey

--- frontend/src/home/CreateSurvey.jsx
+++ frontend/src/home/CreateSurvey.jsx
@@ -2,23 +2,20 @@ import { useState, useRef, useContext, useEffect } from 'react';
 import { Toast } from 'primereact/toast';
 import { Button } from 'primereact/button';
 import { InputText } from 'primereact/inputtext';
-import { getSurvey } from "../api/surveyApi.js";
-import { SurveyContext } from "../context/SurveyContext.jsx";
 import { UserContext } from "../context/UserContext.jsx";
 import Home from '../home/Home.jsx';
-import {logoutUser, registerUser} from "../api/userApi.js";
-import {Accordion, AccordionTab} from "primereact/accordion";
-import {Dropdown} from "primereact/dropdown";
+import Question from "./Question.jsx";
 
 const CreateSurvey = () => {
     const toast = useRef(null);
+    const lastQuestionId = useRef(1)
     const [user] = useContext(UserContext);
     const [showHome, setShowHome] = useState(false);
+    const [questionList, setQuestionList] = useState([<Question key={1} id={1}/>])
 
-
-    // useEffect(() => {
-    //     setShowHome(!!(user || survey));
-    // }, [user, survey]);
+    const getUniqueId = () =>  {
+        return ++lastQuestionId.current;
+    }
 
 
     if (showHome) {
@@ -29,11 +26,22 @@ const CreateSurvey = () => {
         //const createStatus = await createSurvey(questionList);
     };
 
+    const handleAddQuestion = () => {
+        const newId = getUniqueId()
+
+        setQuestionList(prevState => {
+            return [...prevState, <Question key={newId} id={newId}/>]
+        })
+    };
+
     return (
         <>
             <Button label="Back to Home" icon="pi pi-home" size="small" className="absolute top-0 left-0 m-4"
                     style={{boxShadow: "none"}} onClick={() => setShowHome(true)}/>
             <div className="flex flex-column align-items-center gap-3 p-4 card">
+                {questionList}
+                <Button label="Add question" icon="pi pi-plus-circle" size="small"
+                        style={{boxShadow: "none"}} onClick={handleAddQuestion}/>
                 <Button label="Create Survey!" icon="pi pi-send" size="small"
                         style={{boxShadow: "none"}} onClick={onSubmit}/>
             </div>
