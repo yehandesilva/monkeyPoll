e9bc3e68562c7d78a2f54f8b30840dd67457990f
Amin Zeina <36240585+AminZeina@users.noreply.github.com>
2024-11-17 22:47:36 -0500

    Fixed bug where displayed numbers didn't match the values that were actually stored in NumberQuestionOptions.jsx (caused when invalid numbers where manually typed into the textboxes)

--- frontend/src/home/NumberQuestionOptions.jsx
+++ frontend/src/home/NumberQuestionOptions.jsx
@@ -4,10 +4,34 @@ import {InputNumber} from "primereact/inputnumber";
 
 const NumberQuestionOptions = ({responseOptions, setResponseOptions}) => {
 
-    const updateResponseOptions = (e, boundToUpdate) => {
-        setResponseOptions(prevState => {
-            return {...prevState, [boundToUpdate]: e.value}
-        })
+
+    const updateMinOption = (e) => {
+        if (!responseOptions["max"] || e.value < responseOptions["max"]) {
+            // either there is no max, or the new value is lower than max, so set min to this value
+            setResponseOptions(prevState => {
+                return {...prevState, min: e.value}
+            })
+        } else {
+            // new value is invalid, so set it to the current max
+
+            setResponseOptions(prevState => {
+                return {...prevState, min: responseOptions["max"]}
+            })
+        }
+    }
+
+    const updateMaxOption = (e) => {
+        if (!responseOptions["min"] || e.value > responseOptions["min"]) {
+            // either there is no min, or the new value is higher than min, so set max to this value
+            setResponseOptions(prevState => {
+                return {...prevState, max: e.value}
+            })
+        } else {
+            // new value is invalid, so set it to the current max
+            setResponseOptions(prevState => {
+                return {...prevState, max: responseOptions["min"] }
+            })
+        }
     }
 
     return (
@@ -16,13 +40,13 @@ const NumberQuestionOptions = ({responseOptions, setResponseOptions}) => {
                 <div className="field col-12 md:col-6">
                     <label htmlFor="min" className="mb-1">Minimum value</label>
                     <InputNumber inputId="min" value={responseOptions["min"]} onChange={(e) =>
-                        updateResponseOptions(e, "min")} showButtons className="w-auto"
+                        updateMinOption(e)} showButtons className="w-auto"
                                  max={responseOptions["max"]}/>
                 </div>
                 <div className="field col-12 md:col-6">
                     <label htmlFor="max" className="mb-1">Maximum value</label>
                     <InputNumber inputId="max" value={responseOptions["max"]} onChange={(e) =>
-                        updateResponseOptions(e, "max")} showButtons className="w-auto"
+                        updateMaxOption(e)} showButtons className="w-auto"
                                  min={responseOptions["min"]}/>
                 </div>
 
