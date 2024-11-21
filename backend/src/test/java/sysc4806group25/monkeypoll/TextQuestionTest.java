13cfecdaa478584cf30d7e65ebd9ff7863f3546e
Yehan De Silva <yehan@fiberdock.ca>
2024-11-21 18:09:57 -0500

    Added tests to test TextResponse class

--- backend/src/test/java/sysc4806group25/monkeypoll/TextQuestionTest.java
+++ backend/src/test/java/sysc4806group25/monkeypoll/TextQuestionTest.java
@@ -12,6 +12,7 @@ import sysc4806group25.monkeypoll.model.TextResponse;
 import sysc4806group25.monkeypoll.repo.AccountRepository;
 import sysc4806group25.monkeypoll.repo.SurveyRepository;
 import sysc4806group25.monkeypoll.repo.TextQuestionRepository;
+import sysc4806group25.monkeypoll.repo.TextResponseRepository;
 
 import java.util.ArrayList;
 
@@ -24,6 +25,8 @@ import static org.junit.Assert.assertEquals;
 @RunWith(SpringRunner.class)
 public class TextQuestionTest {
 
+    @Autowired
+    private TextResponseRepository textResponseRepository;
     @Autowired
     private TextQuestionRepository textQuestionRepository;
     @Autowired
@@ -72,7 +75,29 @@ public class TextQuestionTest {
     }
 
     /**
-     * Test persistence of text question record in database.
+     * Test the response statement of a response.
+     */
+    @Test
+    public void testResponse() {
+        TextResponse response = new TextResponse();
+        String newResponse = "New response";
+        response.setResponse(newResponse);
+        assertEquals(newResponse, response.getResponse());
+    }
+
+    /**
+     * Test the question of a response.
+     */
+    @Test
+    public void testResponseQuestion() {
+        TextResponse response = new TextResponse();
+        TextQuestion question = new TextQuestion();
+        response.setQuestion(question);
+        assertEquals(question, response.getQuestion());
+    }
+
+    /**
+     * Test persistence of text question and response record in database.
      */
     @Test
     public void testPersistence() {
@@ -83,6 +108,8 @@ public class TextQuestionTest {
 
         ArrayList<TextQuestion> persistedTextQuestions = new ArrayList<>();
         textQuestionRepository.findAll().forEach(persistedTextQuestions::add);
+        ArrayList<TextResponse> persistedTextResponses = new ArrayList<>();
+        textResponseRepository.findAll().forEach(persistedTextResponses::add);
 
         // Save two text question instances
         TextQuestion question1 = new TextQuestion("Question 1", survey);
@@ -90,10 +117,25 @@ public class TextQuestionTest {
         textQuestionRepository.save(question1);
         textQuestionRepository.save(question2);
 
-        // Verify surveys are persisted
-        int initialSize = persistedTextQuestions.size();
+        // Add responses
+        TextResponse response1 = new TextResponse();
+        TextResponse response2 = new TextResponse();
+        question1.addResponse(response1);
+        question2.addResponse(response2);
+
+        // Verify questions are persisted
+        int initialQuestionSize = persistedTextQuestions.size();
         persistedTextQuestions.clear();
         textQuestionRepository.findAll().forEach(persistedTextQuestions::add);
-        assertEquals(initialSize + 2, persistedTextQuestions.size());
+        assertEquals(initialQuestionSize + 2, persistedTextQuestions.size());
+
+        // Verify responses are persisted
+        int initialResponseSize = persistedTextResponses.size();
+        persistedTextResponses.clear();
+        textResponseRepository.findAll().forEach(persistedTextResponses::add);
+        assertEquals(initialResponseSize + 2, persistedTextQuestions.size());
+        for (TextResponse response : persistedTextResponses) {
+            assertEquals(question1, response.getQuestion());
+        }
     }
 }
