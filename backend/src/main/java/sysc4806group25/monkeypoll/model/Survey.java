8cf47c8aee507ac23131e87311c94471025d91aa
Yehan De Silva <yehan@fiberdock.ca>
2024-11-21 07:51:15 -0500

    Added javadoc comments to account and survey

--- backend/src/main/java/sysc4806group25/monkeypoll/model/Survey.java
+++ backend/src/main/java/sysc4806group25/monkeypoll/model/Survey.java
@@ -2,10 +2,18 @@ package sysc4806group25.monkeypoll.model;
 
 import jakarta.persistence.*;
 
+/**
+ * The Survey class models the survey entity for the MonkeyPoll application.
+ * Users can create surveys composed of multiple survey questions and share them with other people to complete.
+ *
+ * @author Yehan De Silva
+ * @date November 21st, 2024
+ */
 @Entity
 @SequenceGenerator(name="surveySeq")
 public class Survey {
 
+    // Fields
     private long surveyId;
     private String description;
     private Boolean closed;
@@ -14,6 +22,9 @@ public class Survey {
     @JoinColumn(name = "accountId", nullable = false)
     private Account account;
 
+    /**
+     * Empty constructor with no args for JPA
+     */
     public Survey() {
 
     }
@@ -26,32 +37,53 @@ public class Survey {
         return this.surveyId;
     }
 
+    /**
+     * @return account who created the survey.
+     */
     public Account getAccount() {
         return this.account;
     }
 
+    /**
+     * @return survey's description.
+     */
     public String getDescription() {
         return this.description;
     }
 
+    /**
+     * @return true if the survey is closed, false otherwise
+     */
     public Boolean getClosed() {
         return this.closed;
     }
 
     // Setter methods
 
+    /**
+     * @param surveyId - survey's unique identifier.
+     */
     public void setSurveyId(long surveyId) {
         this.surveyId = surveyId;
     }
 
+    /**
+     * @param account - account the survey belongs to.
+     */
     public void setAccount(Account account) {
         this.account = account;
     }
 
+    /**
+     * @param description - description of the survey.
+     */
     public void setDescription(String description) {
         this.description = description;
     }
 
+    /**
+     * @param closed - true if the survey is closed, false otherwise.
+     */
     public void setClosed(Boolean closed) {
         this.closed = closed;
     }
