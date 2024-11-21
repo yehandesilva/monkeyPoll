6fc00060fc5be8d1981918bd648c5858f2e448eb
Yehan De Silva <yehan@fiberdock.ca>
2024-11-21 09:31:30 -0500

    Created TextResponse entity
    TextResponse encapsulates a user response to a TextQuestion.

--- backend/src/main/java/sysc4806group25/monkeypoll/model/TextQuestion.java
+++ backend/src/main/java/sysc4806group25/monkeypoll/model/TextQuestion.java
@@ -1,11 +1,38 @@
 package sysc4806group25.monkeypoll.model;
 
+import jakarta.persistence.CascadeType;
 import jakarta.persistence.Entity;
+import jakarta.persistence.OneToMany;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Entity
 public class TextQuestion extends Question {
 
-    public TextQuestion() {
+    // Fields
+    private List<TextResponse> responses = new ArrayList<>();
 
+    // GETTERS
+
+    @OneToMany(mappedBy = "question", cascade = CascadeType.ALL, orphanRemoval = true)
+    public List<TextResponse> getResponses() {
+        return responses;
+    }
+
+    // SETTERS
+
+    public void setResponses(List<TextResponse> responses) {
+        this.responses = responses;
+    }
+
+    public void addResponse(TextResponse response) {
+        this.responses.add(response);
+        response.setQuestion(this);
+    }
+
+    public void removeResponse(TextResponse response) {
+        this.responses.remove(response);
+        response.setQuestion(null);
     }
 }
