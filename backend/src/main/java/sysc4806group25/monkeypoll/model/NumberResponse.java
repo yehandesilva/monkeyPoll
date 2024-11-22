ae209cca252743b8009324f747eaa1d73ebac1a9
Yehan De Silva <yehan@fiberdock.ca>
2024-11-21 19:26:59 -0500

    Added documentation to Number Question/Response classes

--- backend/src/main/java/sysc4806group25/monkeypoll/model/NumberResponse.java
+++ backend/src/main/java/sysc4806group25/monkeypoll/model/NumberResponse.java
@@ -4,6 +4,13 @@ import jakarta.persistence.*;
 
 import java.io.Serializable;
 
+/**
+ * The NumberResponse class models the number response entity for the MonkeyPoll application.
+ * A user can respond to a text question with a number response within the given range.
+ *
+ * @author Yehan De Silva
+ * @date November 21st, 2024
+ */
 @Entity
 @SequenceGenerator(name="numberResponseSeq")
 public class NumberResponse implements Serializable {
@@ -20,28 +27,46 @@ public class NumberResponse implements Serializable {
 
     // GETTERS
 
+    /**
+     * @return unique identifier for the response.
+     */
     public long getResponseId() {
         return responseId;
     }
 
+    /**
+     * @return user response to number question.
+     */
     public int getResponse() {
         return response;
     }
 
+    /**
+     * @return corresponding question to response.
+     */
     public NumberQuestion getQuestion() {
         return question;
     }
 
     // SETTERS
 
+    /**
+     * @param responseId - unique identifier for the response.
+     */
     public void setResponseId(long responseId) {
         this.responseId = responseId;
     }
 
+    /**
+     * @param response - user response to number question.
+     */
     public void setResponse(int response) {
         this.response = response;
     }
 
+    /**
+     * @param question - corresponding question to response.
+     */
     public void setQuestion(NumberQuestion question) {
         this.question = question;
     }
