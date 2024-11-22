43ba3c4f4c3ce9886522f52756f2858908895548
Yehan De Silva <yehan@fiberdock.ca>
2024-11-21 19:31:19 -0500

    Added documentation to choice classes

--- backend/src/main/java/sysc4806group25/monkeypoll/model/ChoiceOption.java
+++ backend/src/main/java/sysc4806group25/monkeypoll/model/ChoiceOption.java
@@ -2,13 +2,20 @@ package sysc4806group25.monkeypoll.model;
 
 import jakarta.persistence.*;
 
-import java.io.Serializable;
-
+/**
+ * The ChoiceOption class models the choice option entity for the MonkeyPoll application.
+ * A choice question requires a list of possible options to choose from.
+ *
+ * @author Yehan De Silva
+ * @date November 21st, 2024
+ */
 @Entity
 @SequenceGenerator(name="choiceOptionSeq")
-public class ChoiceOption implements Serializable {
+public class ChoiceOption {
 
     // Fields
+    @Id
+    @GeneratedValue(generator="choiceOptionSeq")
     private long choiceOptionId;
     private String description;
 
@@ -18,30 +25,46 @@ public class ChoiceOption implements Serializable {
 
     // GETTERS
 
-    @Id
-    @GeneratedValue(generator="choiceOptionSeq")
+    /**
+     * @return unique identifier for choice option.
+     */
     public long getChoiceOptionId() {
         return choiceOptionId;
     }
 
+    /**
+     * @return description of the choice option.
+     */
     public String getDescription() {
         return description;
     }
 
+    /**
+     * @return corresponding question of the choice option.
+     */
     public ChoiceQuestion getQuestion() {
         return question;
     }
 
     // SETTERS
 
+    /**
+     * @param choiceOptionId - unique identifier for choice option.
+     */
     public void setChoiceOptionId(long choiceOptionId) {
         this.choiceOptionId = choiceOptionId;
     }
 
+    /**
+     * @param description - description of the choice option.
+     */
     public void setDescription(String description) {
         this.description = description;
     }
 
+    /**
+     * @param question - corresponding question of the choice option.
+     */
     public void setQuestion(ChoiceQuestion question) {
         this.question = question;
     }
