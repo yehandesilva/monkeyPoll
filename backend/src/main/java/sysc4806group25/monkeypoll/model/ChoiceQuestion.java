2477bfe1f3469715ab38ce2aad649a5ca521ef7d
Yehan De Silva <yehan@fiberdock.ca>
2024-11-21 19:31:19 -0500

    Added documentation to choice classes

--- backend/src/main/java/sysc4806group25/monkeypoll/model/ChoiceQuestion.java
+++ backend/src/main/java/sysc4806group25/monkeypoll/model/ChoiceQuestion.java
@@ -7,50 +7,97 @@ import jakarta.persistence.OneToMany;
 import java.util.ArrayList;
 import java.util.List;
 
+/**
+ * The ChoiceQuestion class models the choice question entity for the MonkeyPoll application.
+ * A type of question that can be asked on a survey is a choice question expecting a user to choose one option from a list
+ * of options.
+ *
+ * @author Yehan De Silva
+ * @date November 21st, 2024
+ */
 @Entity
 public class ChoiceQuestion extends Question {
 
     // Fields
+    @OneToMany(mappedBy = "question", cascade = CascadeType.ALL, orphanRemoval = true)
     private List<ChoiceOption> options = new ArrayList<>();
+
+    @OneToMany(mappedBy = "question", cascade = CascadeType.ALL, orphanRemoval = true)
     private List<ChoiceResponse> responses = new ArrayList<>();
+
+    /**
+     * Empty constructor with no args for JPA
+     */
+    public ChoiceQuestion() {
+        super();
+    }
+
+    /**
+     * Constructor for testing purposes
+     */
+    public ChoiceQuestion(String question, Survey survey) {
+        super(question, survey);
+    }
 
     // GETTERS
 
-    @OneToMany(mappedBy = "question", cascade = CascadeType.ALL, orphanRemoval = true)
+    /**
+     * @return options user can choose from.
+     */
     public List<ChoiceOption> getOptions() {
         return options;
     }
 
-    @OneToMany(mappedBy = "question", cascade = CascadeType.ALL, orphanRemoval = true)
+    /**
+     * @return responses to the choice question.
+     */
     public List<ChoiceResponse> getResponses() {
         return responses;
     }
 
     // SETTERS
 
+    /**
+     * @param options - options user can choose from.
+     */
     public void setOptions(List<ChoiceOption> options) {
         this.options = options;
     }
 
+    /**
+     * @param option - option to add to question.
+     */
     public void addOption(ChoiceOption option) {
         this.options.add(option);
         option.setQuestion(this);
     }
 
+    /**
+     * @param option - option to remove from question.
+     */
     public void removeOption(ChoiceOption option) {
         this.options.remove(option);
         option.setQuestion(null);
     }
 
+    /**
+     * @param responses - responses to the choice question.
+     */
     public void setResponses(List<ChoiceResponse> responses) {
         this.responses = responses;
     }
 
+    /**
+     * @param response - response to add to the question.
+     */
     public void addResponse(ChoiceResponse response) {
         this.responses.add(response);
         response.setQuestion(this);
     }
 
+    /**
+     * @param response - response to remove from the question.
+     */
     public void removeResponse(ChoiceResponse response) {
         this.responses.remove(response);
         response.setQuestion(null);
--- backend/src/main/java/sysc4806group25/monkeypoll/model/ChoiceResponse.java
+++ backend/src/main/java/sysc4806group25/monkeypoll/model/ChoiceResponse.java
@@ -2,13 +2,20 @@ package sysc4806group25.monkeypoll.model;
 
 import jakarta.persistence.*;
 
-import java.io.Serializable;
-
+/**
+ * The ChoiceResponse class models the choice response entity for the MonkeyPoll application.
+ * A user can respond to a choice question with a choice response from the given options.
+ *
+ * @author Yehan De Silva
+ * @date November 21st, 2024
+ */
 @Entity
 @SequenceGenerator(name="choiceResponseSeq")
-public class ChoiceResponse implements Serializable {
+public class ChoiceResponse {
 
     // Fields
+    @Id
+    @GeneratedValue(generator = "choiceResponseSeq")
     private long responseId;
     private long response;
 
@@ -18,30 +25,46 @@ public class ChoiceResponse implements Serializable {
 
     // GETTERS
 
-    @Id
-    @GeneratedValue(generator = "choiceResponseSeq")
+    /**
+     * @return unique identifier for the response.
+     */
     public long getResponseId() {
         return responseId;
     }
 
+    /**
+     * @return user response to number question.
+     */
     public long getResponse() {
         return response;
     }
 
+    /**
+     * @return corresponding question to response.
+     */
     public ChoiceQuestion getQuestion() {
         return question;
     }
 
     // SETTERS
 
+    /**
+     * @param responseId - unique identifier for the response.
+     */
     public void setResponseId(long responseId) {
         this.responseId = responseId;
     }
 
+    /**
+     * @param response - user response to number question.
+     */
     public void setResponse(long response) {
         this.response = response;
     }
 
+    /**
+     * @param question - corresponding question to response.
+     */
     public void setQuestion(ChoiceQuestion question) {
         this.question = question;
     }
