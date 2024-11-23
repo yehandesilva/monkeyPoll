d8619d8f1e1609d01cd1da09b129d1843b154bfb
MichaelKyrollos <83596468+MichaelKyrollos@users.noreply.github.com>
2024-11-22 19:30:34 -0500

    Revert "initial update"
    This reverts commit a8edcb409f0ba63399ac4c40c18e123a1d6d0237.

--- backend/src/main/java/sysc4806group25/monkeypoll/controller/AccountController.java
+++ backend/src/main/java/sysc4806group25/monkeypoll/controller/AccountController.java
@@ -14,15 +14,12 @@ import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.context.SecurityContextHolderStrategy;
 import org.springframework.security.web.context.HttpSessionSecurityContextRepository;
 import org.springframework.security.web.context.SecurityContextRepository;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.server.ResponseStatusException;
 import sysc4806group25.monkeypoll.model.Account;
-import sysc4806group25.monkeypoll.model.Survey;
 import sysc4806group25.monkeypoll.service.AccountUserDetailsService;
-import sysc4806group25.monkeypoll.service.SurveyService;
-
-
-import java.util.Optional;
 
 @RestController
 public class AccountController {
@@ -32,9 +29,6 @@ public class AccountController {
 
     @Autowired
     AccountUserDetailsService accountUserDetailsService;
-
-    @Autowired
-    SurveyService surveyService;
 
     private SecurityContextRepository securityContextRepository =
             new HttpSessionSecurityContextRepository();
@@ -75,18 +69,6 @@ public class AccountController {
         throw new ResponseStatusException(HttpStatus.CONFLICT, "An account with that email already exists.");
     }
 
-    @GetMapping("/{userId}/survey/{surveyId}")
-    public ResponseEntity<Survey> getSurveyById(@PathVariable long surveyId, @PathVariable long userId) {
-        Optional<Survey> survey = surveyService.getSurveyById(surveyId);
-        if (survey.isPresent() && survey.get().getAccount().getId() == userId) {
-            return ResponseEntity.ok(survey.get());
-        } else {
-            return ResponseEntity.status(HttpStatus.NOT_FOUND).build();
-        }
-    }
-
     public record LoginRequest(String email, String password) {}
     public record RegisterRequest(String email, String password, String firstName, String lastName) {}
 }
-
-
