2e15d0b9063f9a164021ba90e5b28e6885afdb22
Pathum Danthanarayana <pathumdanthanarayana@cmail.carleton.ca>
2024-11-22 21:32:03 -0500

    Created initial Survey component

---
+++ frontend/src/survey/Survey.jsx
@@ -0,0 +1,14 @@
+
+
+/*
+The Survey component models a Survey that the user can fill
+out and submit.
+ */
+const Survey = () => {
+    return (
+        <div>
+            <h1>Testing, this is the survey page! </h1>
+        </div>
+    )
+}
+export default Survey;
