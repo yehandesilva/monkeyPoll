7fcb5e5a5a4fb77f917680ac41fa5c78d81b782a
Pathum Danthanarayana <pathumdanthanarayana@cmail.carleton.ca>
2024-11-23 05:39:59 -0500

    Reworked how state of response for each question is saved
    - Originally, a single useState hook was used for each dynamically created question
    - Since this clearly did not end up working, a Map was used instead to maintain a key-value pair <questionId, response> for each question. To get the response value for a question, responses.get<questionId> can be used; setResponses() can be used to set the responses. The onChange event for each input field will fire the handleResponseChange function, which will create a new Map using the previous Map, and update the value of the corresponding question (via questionId). A new Map must be created each time to force a re-render.

--- frontend/src/survey/Survey.jsx
+++ frontend/src/survey/Survey.jsx
@@ -16,21 +16,31 @@ out and submit.
 const Survey = () => {
     // Get reference to survey
     const [survey] = useContext(SurveyContext);
-    const [textValue, setTextValue] = useState('');
-    const [numValue, setNumValue] = useState(0);
-    const [option, setOption] = useState('');
+    const surveyQuestions = survey.questions;
+    // Sort the array of questions based on questionId
+    surveyQuestions.sort((a, b) => a.questionId - b.questionId);
 
     const toast = useRef(null);
     const [showMainPage, setShowMainPage] = useState(false);
 
-    let surveyQuestions = survey.questions;
-    // Sort the array of questions based on questionId
-    surveyQuestions.sort((a, b) => a.questionId - b.questionId);
+    // Create useState hook to keep track of the user's response for each question
+    // for each question using a Map -> [questionId, response] is the key-value pair
+    const [responses, setResponses] = useState(new Map(surveyQuestions.map((question) => [question.questionId, null])));
+    // Handle response change by updating the Map with the new value for the key
+    const handleResponseChange = (e, questionId) => {
+        const updatedResponse = e.target.value;
+        console.log("Target value: " + updatedResponse);
+        console.log("Question id: " + questionId);
+        // New map will be created using the previous map, and setting the new value for the key
+        setResponses((originalResponses) => new Map(originalResponses).set(questionId, updatedResponse));
+        console.log(responses.entries());
+    }
 
     // Hook to direct user back to main MonkeyPoll page after survey submission
     useEffect(() => {
         setShowMainPage(!!showMainPage);
     }, [showMainPage]);
+
 
     // Handling clicking of submit button
     const surveySubmit = async () => {
@@ -81,14 +91,16 @@ const Survey = () => {
                 </div>
                 <div className="flex flex-column align-items-center gap-5 pt-5">
                     {surveyQuestions.map(function(question) {
+                        let questionId = question.questionId;
+                        let questionStr = question.question;
                         // Question is a TextQuestion
                         if (question.type === "TextQuestion") {
                             return (
                                 <div key={question.id} className="w-full">
-                                    <Card title={"Question " + question.questionId}>
-                                        <h3>{question.question}</h3>
-                                        <InputTextarea autoResize value={textValue}
-                                                       onChange={(e) => setTextValue(e.target.value)} rows={10} cols={40}/>
+                                    <Card title={"Question " + questionId}>
+                                        <h3>{questionStr}</h3>
+                                        <InputTextarea autoResize value={responses.get(questionId)}
+                                                       onChange={(e) => handleResponseChange(e, questionId)} rows={10} cols={40}/>
                                     </Card>
                                 </div>
                             );
@@ -97,12 +109,12 @@ const Survey = () => {
                         if (question.type === "NumberQuestion") {
                             return (
                                 <div key={question.id} className="w-full">
-                                    <Card title={"Question " + question.questionId}>
-                                        <h3>{question.question}</h3>
+                                    <Card title={"Question " + questionId}>
+                                        <h3>{questionStr}</h3>
                                         <div className="flex-auto">
                                             <label htmlFor="minmax" className="block mb-2">Enter a number between {question.minValue} and {question.maxValue}</label>
-                                            <InputNumber inputId="minmax" value={numValue}
-                                                         onValueChange={(e) => setNumValue(e.value)} min={question.minValue}
+                                            <InputNumber inputId="minmax" value={responses.get(questionId)}
+                                                         onValueChange={(e) => handleResponseChange(e, questionId)} min={question.minValue}
                                                          max={question.maxValue}/>
                                         </div>
                                     </Card>
@@ -114,14 +126,14 @@ const Survey = () => {
                             let choiceOptions = question.options;
                             return (
                                 <div key={question.id} className="w-full">
-                                    <Card title={"Question " + question.questionId}>
-                                        <h3>{question.question}</h3>
+                                    <Card title={"Question " + questionId}>
+                                        <h3>{questionStr}</h3>
                                         <div className="flex flex-column gap-2">
                                             {choiceOptions.map(function(choiceOption) {
                                                 // Create RadioButton for each option
                                                 return (
                                                     <div key={choiceOption.id} className="flex align-items-center">
-                                                        <RadioButton inputId={choiceOption.id} name={question.question} value={choiceOption.description} onChange={(e) => setOption(e.value)} checked={option === choiceOption.description} />
+                                                        <RadioButton inputId={choiceOption.id} name={question} value={choiceOption.description} onChange={(e) => handleResponseChange(e, questionId)} checked={responses.get(questionId) === choiceOption.description} />
                                                         <label htmlFor={choiceOption.id} className="ml-2">{choiceOption.description}</label>
                                                     </div>
                                                 );
