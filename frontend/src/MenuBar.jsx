0bfde68be9f7ffeb035e778076425dfac6fc43c8
Yehan De Silva <yehan@fiberdock.ca>
2024-11-26 09:53:40 -0500

    Added Account sidebar

--- frontend/src/MenuBar.jsx
+++ frontend/src/MenuBar.jsx
@@ -1,14 +1,17 @@
 import {useContext, useState} from 'react';
 
 import {Button} from 'primereact/button';
-import { Avatar } from 'primereact/avatar';
+import {Avatar} from 'primereact/avatar';
+import {Sidebar} from 'primereact/sidebar';
 
 import LoginDialog from "./welcome/LoginDialog.jsx";
 import {UserContext} from "./context/UserContext.jsx";
 import {logoutUser} from "./api/userApi.js";
+import Account from "./home/Account.jsx";
 
 const MenuBar = ({toast}) => {
     const [loginVisible, setLoginVisible] = useState(false);
+    const [sidebarVisible, setSidebarVisible] = useState(false);
     const [user, setUser] = useContext(UserContext);
 
     const handleLogout = async () => {
@@ -29,12 +32,16 @@ const MenuBar = ({toast}) => {
     return (
         <>
             <LoginDialog visible={loginVisible} setVisible={setLoginVisible}/>
+            <Sidebar className="w-full w-25rem" visible={sidebarVisible} onHide={() => setSidebarVisible(false)} position="right">
+                <Account/>
+            </Sidebar>
             <div className="absolute top-0 right-0 m-4">
                 {user ? (
                     <div className="flex gap-2 align-items-center">
                         <Button label="Logout" icon="pi pi-sign-out" size="small"
                                 style={{boxShadow: "none"}} onClick={handleLogout}/>
-                        <Avatar className="flex bg-primary" label={user.firstName.substring(0, 1)} shape="circle" size="large" onClick={() => {}}/>
+                        <Avatar className="flex bg-primary" label={user.firstName.substring(0, 1)} shape="circle"
+                                size="large" onClick={() => setSidebarVisible(true)}/>
                     </div>
                 ) : (
                     <Button label="Login" icon="pi pi-sign-in" size="small"
