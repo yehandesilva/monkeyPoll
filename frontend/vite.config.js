ee8a5f9e4a91cb5d48e56a2dff2a63f3842be79f
Yehan De Silva <yehan@fiberdock.ca>
2024-11-26 07:56:04 -0500

    Added proxy for survey endpoint

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -28,6 +28,11 @@ export default defineConfig({
         target: 'http://localhost:8080',
         changeOrigin: true,
         secure: false
+      },
+      "/survey": {
+        target: 'http://localhost:8080',
+        changeOrigin: true,
+        secure: false
       }
     }
   }
